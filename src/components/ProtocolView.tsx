77bfdc0e59466ef83e2c5adef5b3175357c2ef66
pitskhela <pitskhelauri.elene@gmail.com>
2021-09-02 06:02:26 +0400

    extracted chart component

--- src/components/ProtocolView.tsx
+++ src/components/ProtocolView.tsx
@@ -1,23 +1,14 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { Box, Text, Spinner, Flex, AbsoluteCenter, Stack } from '@chakra-ui/react'
+import { Box, Text, Spinner, Flex, AbsoluteCenter } from '@chakra-ui/react'
 import ReactMarkdown from 'react-markdown'
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer'
-import { format } from 'date-fns'
-import {
-  Annotation,
-  AnnotationLabel,
-  Axis,
-  Grid,
-  LineSeries,
-  Tooltip,
-  XYChart,
-} from '@visx/xychart'
+
 import { LegendOrdinal } from '@visx/legend'
 import { scaleOrdinal } from '@visx/scale'
 
 import { useProposalQuery, useProposalVotesQuery } from '../queries'
-import { components } from '../types/schema/swagger'
+import VoteChart from './VoteChart'
 
 interface ChartProps {
   counter?: number
@@ -33,27 +24,8 @@ const ProtocolView: React.VFC<ChartProps> = (props) => {
 
   const proposal = useProposalQuery(refId)
   const votes = useProposalVotesQuery(proposal.data?.refId)
-  console.log(proposal.data)
 
   if (!proposal || votes.isError || !votes.data) return null
-
-  const nayVotes = votes.data.filter((vote) => vote.choice === 0)
-  const yayVotes = votes.data.filter((vote) => vote.choice === 1)
-
-  console.log(nayVotes)
-
-  const nayData = getChartData(nayVotes)
-  const yayData = getChartData(yayVotes)
-
-  const dateRange = nayData.concat(yayData).map((data) => data.x)
-  const voteRange = nayData.concat(yayData).map((data) => data.y)
-
-  const quorumNeeded = Math.max(...voteRange) / 2
-
-  const quorumData = [
-    { x: Math.min(...dateRange), y: quorumNeeded },
-    { x: Math.max(...dateRange), y: quorumNeeded },
-  ]
 
   const ordinalColorScale = scaleOrdinal({
     domain: ['Yay', 'Nay'],
@@ -102,86 +74,7 @@ const ProtocolView: React.VFC<ChartProps> = (props) => {
           />
         </Flex>
 
-        <XYChart
-          height={500}
-          margin={{ top: 10, right: 80, bottom: 80, left: 80 }}
-          xScale={{ type: 'time' }}
-          yScale={{ type: 'linear', nice: true, round: true }}
-        >
-          <Grid columns={false} numTicks={5} lineStyle={{ stroke: '#EBF0F6' }} />
-          <Axis
-            orientation="left"
-            numTicks={5}
-            tickFormat={powerToFormatted}
-            hideAxisLine
-            hideTicks
-            tickLabelProps={() => ({ fill: '#A2ACBA', fontWeight: 'bold' })}
-          />
-          <Axis
-            orientation="bottom"
-            numTicks={5}
-            tickFormat={timestampToFormatted}
-            hideAxisLine
-            hideTicks
-            tickLabelProps={() => ({
-              fill: '#A2ACBA',
-              fontWeight: 'bold',
-              transform: 'translate(0, 13)',
-            })}
-          />
-          <LineSeries
-            dataKey="Nay"
-            data={nayData}
-            xAccessor={(d) => d?.x}
-            yAccessor={(d) => d?.y}
-            stroke="#F44061"
-          />
-          <LineSeries
-            dataKey="Yay"
-            data={yayData}
-            xAccessor={(d) => d?.x}
-            yAccessor={(d) => d?.y}
-            stroke="#25C9A1"
-          />
-          <LineSeries
-            dataKey="Quorum"
-            data={quorumData}
-            xAccessor={(d) => d?.x}
-            yAccessor={(d) => d?.y}
-            stroke="#728096"
-            strokeWidth="2"
-            strokeDasharray="1 4"
-          />
-          <Annotation dataKey="Quorum" datum={quorumData[1]}>
-            <AnnotationLabel
-              title="Quorum Needed"
-              showAnchorLine={false}
-              backgroundFill="transparent"
-              verticalAnchor="start"
-              width={110}
-              titleProps={{ fill: '#728096' }}
-            />
-          </Annotation>
-          <Tooltip<typeof yayData[0]>
-            snapTooltipToDatumX
-            snapTooltipToDatumY
-            showSeriesGlyphs
-            renderTooltip={({ tooltipData, colorScale }) =>
-              tooltipData &&
-              tooltipData.nearestDatum &&
-              tooltipData.nearestDatum.key !== 'Quorum' ? (
-                <Stack padding={2} spacing={1}>
-                  <Box>Address: {tooltipData.nearestDatum.datum.address}</Box>
-                  <Box>Vote: {tooltipData.nearestDatum.key}</Box>
-                  <Box>Power: {tooltipData.nearestDatum.datum.y}</Box>
-                  <Box>
-                    Time: {timestampToFormatted(new Date(tooltipData.nearestDatum.datum.x!))}
-                  </Box>
-                </Stack>
-              ) : null
-            }
-          />
-        </XYChart>
+        <VoteChart votes={votes.data} />
       </Box>
 
       {proposal.data.content ? (
@@ -191,39 +84,6 @@ const ProtocolView: React.VFC<ChartProps> = (props) => {
       ) : null}
     </Box>
   )
-}
-
-const getChartData = (votes: components['schemas']['Vote'][]) => {
-  return votes
-    .map((vote) => {
-      return [vote.timestamp!, vote.power!, vote.address!]
-    })
-    .sort((a, b) => {
-      return (a[0] as number) - (b[0] as number)
-    })
-    .reduce((acc, c) => {
-      const lastVote = acc[acc.length - 1]
-      const lastPower = lastVote ? lastVote[1] : 0
-      const vote: [number, number, string] = [
-        c[0] as number,
-        (c[1] as number) + lastPower,
-        c[2] as string,
-      ]
-
-      return acc.concat([vote])
-    }, [] as Array<[number, number, string]>)
-    .map(([timestamp, aggregatedPower, address]) => {
-      return { x: timestamp, y: aggregatedPower, address: address }
-    })
-}
-
-const timestampToFormatted = (date: Date) => {
-  return format(date, 'MMM do HH:mm')
-}
-
-const powerToFormatted = (power: number) => {
-  const formatter = Intl.NumberFormat('en', { notation: 'compact' })
-  return formatter.format(power)
 }
 
 export default ProtocolView
---
+++ src/components/VoteChart.tsx
@@ -0,0 +1,150 @@
+import {
+  Annotation,
+  AnnotationLabel,
+  Axis,
+  Grid,
+  LineSeries,
+  Tooltip,
+  XYChart,
+} from '@visx/xychart'
+import { Box, Stack } from '@chakra-ui/react'
+import React from 'react'
+
+import { components } from '../types/schema/swagger'
+import { format } from 'date-fns'
+
+interface VoteChartProps {
+  votes: components['schemas']['Vote'][]
+}
+
+const VoteChart: React.VFC<VoteChartProps> = ({ votes }) => {
+  const nayVotes = votes.filter((vote) => vote.choice === 0)
+  const yayVotes = votes.filter((vote) => vote.choice === 1)
+
+  const nayData = getChartData(nayVotes)
+  const yayData = getChartData(yayVotes)
+
+  const dateRange = nayData.concat(yayData).map((data) => data.x)
+  const voteRange = nayData.concat(yayData).map((data) => data.y)
+
+  const quorumNeeded = Math.max(...voteRange) / 2
+
+  const quorumData = [
+    { x: Math.min(...dateRange), y: quorumNeeded },
+    { x: Math.max(...dateRange), y: quorumNeeded },
+  ]
+
+  return (
+    <XYChart
+      height={500}
+      margin={{ top: 10, right: 80, bottom: 80, left: 80 }}
+      xScale={{ type: 'time' }}
+      yScale={{ type: 'linear', nice: true, round: true }}
+    >
+      <Grid columns={false} numTicks={5} lineStyle={{ stroke: '#EBF0F6' }} />
+      <Axis
+        orientation="left"
+        numTicks={5}
+        tickFormat={powerToFormatted}
+        hideAxisLine
+        hideTicks
+        tickLabelProps={() => ({ fill: '#A2ACBA', fontWeight: 'bold' })}
+      />
+      <Axis
+        orientation="bottom"
+        numTicks={5}
+        tickFormat={timestampToFormatted}
+        hideAxisLine
+        hideTicks
+        tickLabelProps={() => ({
+          fill: '#A2ACBA',
+          fontWeight: 'bold',
+          transform: 'translate(0, 13)',
+        })}
+      />
+      <LineSeries
+        dataKey="Nay"
+        data={nayData}
+        xAccessor={(d) => d?.x}
+        yAccessor={(d) => d?.y}
+        stroke="#F44061"
+      />
+      <LineSeries
+        dataKey="Yay"
+        data={yayData}
+        xAccessor={(d) => d?.x}
+        yAccessor={(d) => d?.y}
+        stroke="#25C9A1"
+      />
+      <LineSeries
+        dataKey="Quorum"
+        data={quorumData}
+        xAccessor={(d) => d?.x}
+        yAccessor={(d) => d?.y}
+        stroke="#728096"
+        strokeWidth="2"
+        strokeDasharray="1 4"
+      />
+      <Annotation dataKey="Quorum" datum={quorumData[1]}>
+        <AnnotationLabel
+          title="Quorum Needed"
+          showAnchorLine={false}
+          backgroundFill="transparent"
+          verticalAnchor="start"
+          width={110}
+          titleProps={{ fill: '#728096' }}
+        />
+      </Annotation>
+      <Tooltip<typeof yayData[0]>
+        snapTooltipToDatumX
+        snapTooltipToDatumY
+        showSeriesGlyphs
+        renderTooltip={({ tooltipData, colorScale }) =>
+          tooltipData && tooltipData.nearestDatum && tooltipData.nearestDatum.key !== 'Quorum' ? (
+            <Stack padding={2} spacing={1}>
+              <Box>Address: {tooltipData.nearestDatum.datum.address}</Box>
+              <Box>Vote: {tooltipData.nearestDatum.key}</Box>
+              <Box>Power: {tooltipData.nearestDatum.datum.y}</Box>
+              <Box>Time: {timestampToFormatted(new Date(tooltipData.nearestDatum.datum.x!))}</Box>
+            </Stack>
+          ) : null
+        }
+      />
+    </XYChart>
+  )
+}
+
+const getChartData = (votes: components['schemas']['Vote'][]) => {
+  return votes
+    .map((vote) => {
+      return [vote.timestamp!, vote.power!, vote.address!]
+    })
+    .sort((a, b) => {
+      return (a[0] as number) - (b[0] as number)
+    })
+    .reduce((acc, c) => {
+      const lastVote = acc[acc.length - 1]
+      const lastPower = lastVote ? lastVote[1] : 0
+      const vote: [number, number, string] = [
+        c[0] as number,
+        (c[1] as number) + lastPower,
+        c[2] as string,
+      ]
+
+      return acc.concat([vote])
+    }, [] as Array<[number, number, string]>)
+    .map(([timestamp, aggregatedPower, address]) => {
+      return { x: timestamp, y: aggregatedPower, address: address }
+    })
+}
+
+const timestampToFormatted = (date: Date) => {
+  return format(date, 'MMM do HH:mm')
+}
+
+const powerToFormatted = (power: number) => {
+  const formatter = Intl.NumberFormat('en', { notation: 'compact' })
+  return formatter.format(power)
+}
+
+export default VoteChart
